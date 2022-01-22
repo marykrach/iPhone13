084b29dc7fa090b6ff8bb98f56c9b8fd3d0dd7f3
marykrach <80330561+marykrach@users.noreply.github.com>
2022-01-22 17:06:26 +0300

    Add files via upload
    add tabs.js

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,38 +1,31 @@
-const links = document.querySelectorAll('.header-menu__item a')
+const scrollFunk = () => {
+  const links = document.querySelectorAll('.header-menu__item a')
+  const linkCharacteristics = document.querySelector('.card-details__link-characteristics')
 
-seamless.polyfill();
+  const newArray = [...links, linkCharacteristics]
 
-links.forEach((element) => {
-  element.addEventListener('click', (event) => {
-    event.preventDefault()
+  seamless.polyfill();
 
-    const id = element.getAttribute('href').substring(1)
-    const section = document.getElementById(id)
+  newArray.forEach((element) => {
+    element.addEventListener('click', (event) => {
+      event.preventDefault()
 
-    if (section) {
-      seamless.elementScrollIntoView(section, {
-        behavior: 'smooth',
-        block: 'start'
-      })
-    } else {
-      seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
-        behavior: "smooth",
-        block: "center",
-        inline: "center",
-      });
-    }
+      const id = element.getAttribute('href').substring(1)
+      const section = document.getElementById(id)
+
+      if (section) {
+        seamless.elementScrollIntoView(section, {
+          behavior: 'smooth',
+          block: 'start'
+        })
+      } else {
+        seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+          behavior: "smooth",
+          block: "center",
+          inline: "center",
+        });
+      }
+    })
   })
-})
-
-const link_characteristics = document.querySelector('.card-details__link-characteristics')
-
-link_characteristics.addEventListener('click', (event) => {
-  event.preventDefault()
-
-  const section = document.getElementById("characteristics")
-
-  seamless.elementScrollIntoView(section, {
-    behavior: 'smooth',
-    block: 'start'
-  });
-})
+}
+scrollFunk()
